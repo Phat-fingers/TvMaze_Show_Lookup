6f9189177138ec0c4a91fbf2f9c24b7bc7bf0294
Phat-Fingers <anders.peterssson@gmail.com>
2020-02-18 14:27:05 +0100

    some small chnages

--- tvmaze.py
+++ tvmaze.py
@@ -1,50 +1,51 @@
 import requests
-import json
-tvinfo_dict = {
+tvinfo = {
     "Show_name": "",
     "Show_url": "",
     "Show_type": "",
     "Show_status": "",
     "Runtime": "",
-    "Next_episode": "",
-    "Season": "",
-    "Episode": ""
+    "Next_episode": "No info",
+    "Season": "No info",
+    "Episode": "No info"
 
 }
 def get_show_info(show):
 
     api = f'http://api.tvmaze.com/singlesearch/shows?q=:{show}'
-    resp = requests.get(api)
-    data = resp.json()
-    da = json.dumps(data, indent=2)
-    tvinfo_dict['Show_name'] = data['name']
-    tvinfo_dict['Show_url'] = data['url']
-    tvinfo_dict['Show_type'] = data['type']
-    tvinfo_dict['Show_status'] = data['status']
-    #for n in data:
-    #     print(n,'<>',data[n])
-    if data['status'] == 'Ended':
-         print(tvinfo_dict['Show_name'] + ' '+ tvinfo_dict['Show_url'] + ' ' + tvinfo_dict['Show_status'])
-    else:
+    response = requests.get(api)
+    data = response.json()
+    #fetching info about the show
+    tvinfo['Show_name'] = data['name']
+    tvinfo['Show_url'] = data['url']
+    tvinfo['Show_type'] = data['type']
+    tvinfo['Show_status'] = data['status']
+    try:
         next_episode = data['_links']['nextepisode']['href']
         nextep = requests.get(next_episode)
         epdata = nextep.json()
-        #print(epdata)
+        # fetching info about the next episode
+        tvinfo['Next_episode'] = epdata['airdate']
+        tvinfo['Season'] = epdata['season']
+        tvinfo['Episode'] = epdata['number']
+        tvinfo['Runtime'] = data['runtime']
+    except:
+        pass
+    #for n in data:
+    #     print(n,'<>',data[n])
 
 
-        tvinfo_dict['Next_episode'] = epdata['airdate']
-        tvinfo_dict['Season'] = epdata['season']
-        tvinfo_dict['Episode'] = epdata['number']
-        tvinfo_dict['Runtime'] = data['runtime']
+    #print(tvinfo_dict['Show_name'])
+    #print(tvinfo_dict['Show_type'])
+    #print(tvinfo_dict['Show_status'])
+    #print(tvinfo_dict['Next_episode'])
+    return tvinfo
 
 
-        # print(tvinfo_dict['Show_name'])
-        # print(tvinfo_dict['Show_type'])
-        # print(tvinfo_dict['Show_status'])
-        # print(tvinfo_dict['Next_episode'])
-        return tvinfo_dict
-
-
-v = input('Search for show')
+v = input('Search for show: ')
 get_show_info(v)
-print(tvinfo_dict)
+print(f"Show: {tvinfo['Show_name']}")
+print(f"Url: {tvinfo['Show_url']}")
+print(f"Show type: {tvinfo['Show_type']}")
+print(f"Show status: {tvinfo['Show_status']}")
+print(f"Season: {tvinfo['Season']}. Episode: {tvinfo['Episode']}. Airs: {tvinfo['Next_episode']}.")
