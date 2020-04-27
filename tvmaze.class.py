97ed496c405817ff04c47cf9817198cf8999b2f2
Phat-fingers <anders.peterssson@gmail.com>
2020-04-27 20:55:11 +0200

    Yep

--- tvmaze.class.py
+++ tvmaze.class.py
@@ -1,6 +1,7 @@
 import requests
 import json
 
+
 class TVMaze:
 
     def getShowInfo(self, show):
@@ -9,11 +10,12 @@ class TVMaze:
         self.response = requests.get(self.url)
         self.data = self.response.json()
         # Grabing info about next episode
-        if self.data['status'] == 'Running':
+        try:
             self.next_ep_url = self.data['_links']['nextepisode']['href']
             self.next_resp = requests.get(self.next_ep_url)
             self.next_data = self.next_resp.json()
-
+        except:
+            pass
 
     def ShowInfo(self, show):
         self.getShowInfo(show)
@@ -22,13 +24,12 @@ class TVMaze:
         print(f"Show Type   : {self.data['type']}")
         print(f"Status      : {self.data['status']}")
         if self.data['status'] == 'Running':
-            print(f"Next Episode: S{self.next_data['season']}E{self.next_data['number']}")
-            print(f"Airs        : {self.next_data['airdate']}")
-
-
-
-
-
+            try:
+                print(f"Next Episode: S{self.next_data['season']}E{self.next_data['number']}")
+                print(f"Airs        : {self.next_data['airdate']}")
+            except:
+                print('Next Episode: N/A')
+                print('Airs        : N/A')
 
     def menu(self):
         print('--------------MENU---------------')
@@ -37,13 +38,11 @@ class TVMaze:
         print('3. Enter date to see schedule (US)')
 
 
-
 if __name__ == "__main__":
     tv = TVMaze()
     while True:
         tv.menu()
         choice = input('Select a choice: ')
         if choice == '1':
-            #show = input('Enter a show: ')
-            tv.ShowInfo('supernatural')
-
+            show = input('Enter a show: ')
+            tv.ShowInfo(show)
