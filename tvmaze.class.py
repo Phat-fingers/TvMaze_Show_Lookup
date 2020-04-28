32adc26611442c0cb8cf5496dfc1480f67d2d7e0
Phat-Fingers <anders.peterssson@gmail.com>
2020-04-28 10:52:49 +0200

    Yep

--- tvmaze.class.py
+++ tvmaze.class.py
@@ -1,13 +1,16 @@
 import requests
-import json
+from datetime import datetime
 
+today = datetime.today()
+today = today.strftime("%Y-%m-%d")
 
 class TVMaze:
 
+
     def getShowInfo(self, show):
         # Grabbing show info
-        self.url = f'http://api.tvmaze.com/singlesearch/shows?q=:{show}'
-        self.response = requests.get(self.url)
+        self.single_search_url = f'http://api.tvmaze.com/singlesearch/shows?q=:{show}'
+        self.response = requests.get(self.single_search_url)
         self.data = self.response.json()
         # Grabing info about next episode
         try:
@@ -15,7 +18,7 @@ class TVMaze:
             self.next_resp = requests.get(self.next_ep_url)
             self.next_data = self.next_resp.json()
         except:
-            pass
+            self.noData = 'No data found'
 
     def ShowInfo(self, show):
         self.getShowInfo(show)
@@ -28,14 +31,27 @@ class TVMaze:
                 print(f"Next Episode: S{self.next_data['season']}E{self.next_data['number']}")
                 print(f"Airs        : {self.next_data['airdate']}")
             except:
-                print('Next Episode: N/A')
-                print('Airs        : N/A')
+                print(f'Next Episode: {self.noData}')
+                print(f'Airs        : {self.noData}')
+
+    def todaysSchedule(self, date):
+        self.todays_url = f'http://api.tvmaze.com/schedule?country=US&date={date}'
+        self.today_resp = requests.get(self.todays_url)
+        self.today_data = self.today_resp.json()
+
+        for show in self.today_data:
+            if show['airtime'] >= '20:00':
+                print('-' * 30)
+                print(f"Tonight at {show['airtime']}: {show['show']['name']}")
+                print(f"Show Type: {show['show']['type']}")
+
 
     def menu(self):
         print('--------------MENU---------------')
         print('1. Lookup a show')
         print('2. Show today´s schedule (US) ')
         print('3. Enter date to see schedule (US)')
+        print('-'*33)
 
 
 if __name__ == "__main__":
@@ -43,6 +59,13 @@ if __name__ == "__main__":
     while True:
         tv.menu()
         choice = input('Select a choice: ')
-        if choice == '1':
+        if choice not in ('1, 2, 3'):
+            print('you must enter a valid number')
+        elif choice == '1':
             show = input('Enter a show: ')
             tv.ShowInfo(show)
+        elif choice == '2':
+            tv.todaysSchedule(today)
+        elif choice == '3':
+            date = input('Enter a date in format (2020-01-01) ')
+            tv.todaysSchedule(date)
